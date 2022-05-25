60ffa7632d988d5d80cf9d21ff38450c868f0bbf
engi-nier <82199028+MO-deus@users.noreply.github.com>
2022-05-25 14:28:59 +0530

    SDL-Event driven program

---
+++ SDL_playground/event_driven_program/event_driven_program.cpp
@@ -0,0 +1,47 @@
+
+#include "handler.h"
+
+int main(int argc, char *argv[])		// for comapatibility with other platforms 
+{
+
+			if (!init())
+			{
+				printf("Unable to create/initialize window ");
+			}
+
+			else
+			{
+				if (!LoadMedia())
+				{
+					printf("Unable to load media. Error : %s \n", SDL_GetError());
+				}
+
+				else
+				{
+					// until the application is running 
+					while (IS_WINDOW_RUNNING)
+					{
+						// SDL_PollEvent() works on an event queue
+						// It reads from that queue and executes until the queue is empty 
+						while (SDL_PollEvent(&G_event_handler) != 0)
+						{
+							if (G_event_handler.type == SDL_QUIT)
+								//SDL_QUIT occurs when user X's out of the window
+							{
+								goto OUT;
+							}
+						}
+										// src			 // destination
+						SDL_BlitSurface(G_surface_img, NULL, Gsurface, NULL);
+						SDL_UpdateWindowSurface(Gwindow);
+
+					}
+				
+				}
+
+			}	
+	
+	OUT :
+	EndSDL();
+	return 0;
+}
---
+++ SDL_playground/event_driven_program/handler.h
@@ -0,0 +1,87 @@
+
+#pragma once
+
+#ifndef SDL_HANDLER_H
+#define SDL_HANDLER_H
+
+
+
+#include<iostream>	// for handling i/o streams and displaying errors
+#include <stdio.h>	// for printing errors on console , use it when using printf()
+#include "SDL.h"
+
+// constants :
+constexpr int WINDOW_RUNNING = 1L;			// 'L' to tell the compiler not to truncate the value, 
+constexpr int WINDOW_HEIGHT = 480L;			// 'L' means long data_type will be used
+constexpr int WINDOW_WIDTH = 640L;
+constexpr bool IS_WINDOW_RUNNING = true;	// flag for window running(true) or not(false)
+
+// global variables : 
+SDL_Window* Gwindow = NULL;				// Declaring the window, which will be used to render 
+SDL_Surface* Gsurface = NULL;			// a surface is a layer containing an image, 
+										// which can be loaded from a file or an image inside the window
+SDL_Surface* G_surface_img = NULL;		// a surface is a layer containing an image
+SDL_Event G_event_handler;	
+
+#include<iostream>	// for handling i/o streams and displaying errors
+#include <stdio.h>	// for printing errors on console , use it when using printf()
+#include "SDL.h"
+
+bool init()
+{
+	bool success_flag = true;
+	// Initializing SDL [for video]
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("Unable to initialize SDL. SDL error : %s\n", SDL_GetError());
+		success_flag = false;
+	}
+
+	else {
+		// creates a window 
+		Gwindow = SDL_CreateWindow("foo's tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+		if (Gwindow == NULL)
+		{
+			printf("Window cannot be created. SDL Error : %s", SDL_GetError());
+			success_flag = false;
+		}
+
+		else {
+			// takes the surface from the current window to write to the surface 
+			Gsurface = SDL_GetWindowSurface(Gwindow);
+		}
+
+		return success_flag;
+	}
+}
+
+bool LoadMedia()
+{
+	bool success_flag = true;
+
+	// Loads an bmp image into the surface
+	// the directory is where .vcxproj is located
+	G_surface_img = SDL_LoadBMP("eugen_abstract.bmp");
+
+	if (G_surface_img == NULL)
+	{
+		printf("Unable to load image. Error : %s", SDL_GetError());
+		success_flag = false;
+	}
+	return success_flag;
+}
+
+bool EndSDL()
+{
+	// destroys the specified window
+	SDL_DestroyWindow(Gwindow);
+
+	// quits all the subsystem 
+	SDL_Quit();
+
+	return true;
+}
+
+
+
+#endif // !SDL_HANDLER_H
